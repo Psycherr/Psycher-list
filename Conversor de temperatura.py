ce621bc8a1be7fd3c099329ccfc90a52e4410ae5
Psycherr <63215313+Psycherr@users.noreply.github.com>
2020-04-05 21:45:22 -0300

    Add files via upload

---
+++ Conversor de temperatura.py
@@ -0,0 +1,3 @@
+temperaturaF = (float(input("digite uma temperatura em Fahrenheit")))
+temperaturaC = (temperaturaF - 32) * 5 / 9
+print("A temperatura em Celsius é:", round(temperaturaC,2))
---
+++ ES.py
@@ -0,0 +1,3 @@
+resultado1 = (float(input("Qual o tempo total ?")))
+resultado2 = (float(input("Qual o tempo do processador ?")))
+print ("O TempoES é:", (resultado1 - resultado2))
